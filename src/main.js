415845e92e2c43cef580a4cb5f9c87d2de43f1a1
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2025-12-17 11:34:08 +0100

    feat: WIP header

--- src/main.js
+++ src/main.js
@@ -11,3 +11,30 @@ import './style.css';
 
 //Lancement FancyBox
 Fancybox.bind("[data-fancybox]", {});
+
+//au chargement du DOM
+document.addEventListener("DOMContentLoaded", () => {
+
+  //gestion des liens actifs dans les menus
+  const currentPath = window.location.pathname;
+  // Sélectionne tous les liens de navigation correspondant au chemin actuel
+  const links = Array.from(document.querySelectorAll("nav a")).filter(link => link.getAttribute("href") === currentPath);
+  links.forEach(link => link.classList.add("active"));
+
+  //gestion du menu responsive
+  const menuToggle = document.querySelector(".menu-toggle .menu-toggle-btn");
+  const menu = document.querySelector("header .navbar nav ul");
+
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+
+  // Optionnel : fermer le menu si on clique sur un lien
+  menu.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth < 960) {
+        menu.classList.remove("open");
+      }
+    });
+  });
+});
