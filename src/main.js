5a036e77f744f3f4beddb966bfb9fcfa10ff2dd3
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2025-12-17 10:00:50 +0100

    feat: init some libs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+
+
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+import 'normalize.css';
 import './style.css';
+
+//Lancement FancyBox
+Fancybox.bind("[data-fancybox]", {});
